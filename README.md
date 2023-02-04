882406e7298037238c83a3468ec50909cfe2c4a4
Drew Meyers <drewsmeyers@gmail.com>
2023-02-04 10:09:07 -0500

    First step done

---
+++ README.md
@@ -0,0 +1,6 @@
+## UVM CS201 Assignment 1
+*Drew Meyers*
+
+### Assignment Details
+
+![assignment outline](/warmup.pdf)
---
+++ warmup.asmeyers.h
@@ -0,0 +1,4 @@
+#define MAXNAMELEN 63
+typedef struct {
+char title[1+MAXNAMLEN]; char artist[1+MAXNAMELEN]; unsigned int year;
+} Song;
